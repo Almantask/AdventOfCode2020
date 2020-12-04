4a9fe4551259afd6a94858f6021635492a0933c9
Almantas Karpavičius <almantusk@gmail.com>
2020-12-05 00:30:19 +0200

    Day3 part2

--- AdventOfCode/AdventOfCode.Tests/Day3Tests.cs
+++ AdventOfCode/AdventOfCode.Tests/Day3Tests.cs
@@ -9,15 +9,34 @@ namespace AdventOfCode.Tests
     public class Day3Tests
     {
         [Theory]
-        [MemberData(nameof(TreesDownTheSlopeExpectations))]
+        [MemberData(nameof(Part1TreesDownTheSlopeExpectations))]
         public void Part1_Solve_Returns_TreesDownTheSlope(string map, int expectedTreesInTheWay)
         {
-            var treesInTheWay = Day3.Part1.Solve(map);
+            var treesInTheWay = Day3.Part1.Solve(map, new Day3.Step(3,1));
 
             treesInTheWay.Should().Be(expectedTreesInTheWay);
         }
 
-        public static IEnumerable<object[]> TreesDownTheSlopeExpectations
+        [Theory]
+        [MemberData(nameof(Part2TreesDownTheSlopeExpectations))]
+        public void Part2_Solve_Returns_TreesDownTheSlope(string map, int expectedTreesInTheWay)
+        {
+            var treesInTheWay = Day3.Part2.Solve(map);
+
+            treesInTheWay.Should().Be(expectedTreesInTheWay);
+        }
+
+        [Theory]
+        [MemberData(nameof(ProductExpectations))]
+        public void Product_Returns_MultipliedNumbers(int[] numbers, long expectedProduct)
+        {
+            var product = numbers.Product(i => i);
+
+            product.Should().Be(expectedProduct);
+        }
+
+
+        public static IEnumerable<object[]> Part1TreesDownTheSlopeExpectations
         {
             get
             {
@@ -26,6 +45,30 @@ namespace AdventOfCode.Tests
                 yield return new object[] {Input("x6NoTreesDown"), 0 };
                 yield return new object[] {Input("x6TreesDown"), 5 };
                 yield return new object[] {Input("x7TreesDown"), 6 };
+            }
+        }
+
+        public static IEnumerable<object[]> Part2TreesDownTheSlopeExpectations
+        {
+            get
+            {
+                yield return new object[] { Input("Default"), 336 };
+                yield return new object[] { Input("OnlyTreesButTooSmall"), 0 };
+                yield return new object[] { Input("x6NoTreesDown"), 0 };
+                yield return new object[] { Input("x6TreesDown"), 1250 };
+                yield return new object[] { Input("x7TreesDown"), 3888 };
+            }
+        }
+
+        public static IEnumerable<object[]> ProductExpectations
+        {
+            get
+            {
+                yield return new object[]{new []{2,3}, 6};
+                yield return new object[] { new[] { 2, 3, 4, 5 }, 120 };
+                yield return new object[] { new[] { int.MaxValue, 2 }, (long)int.MaxValue * 2 };
+                yield return new object[]{new []{1,0}, 0};
+                yield return new object[]{new []{2}, 2};
             }
         }
 
--- AdventOfCode/AdventOfCode/D3/Day3.cs
+++ AdventOfCode/AdventOfCode/D3/Day3.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace AdventOfCode.D3
@@ -11,15 +12,12 @@ namespace AdventOfCode.D3
         {
             var passwordsAndPolicies = File.ReadAllText("D3/Input.txt");
 
-            Console.WriteLine("D3P1 answer: " + Part1.Solve(passwordsAndPolicies));
+            Console.WriteLine("D3P1 answer: " + Part1.Solve(passwordsAndPolicies, new Step(3,1)));
             Console.WriteLine("D3P2 answer: " + Part2.Solve(passwordsAndPolicies));
         }
 
         public static class Part1
         {
-            const int stepsDown = 1;
-            const int stepsRight = 3;
-
             /// <summary>
             /// Map of a mountain repeats itself to the right.
             /// It's either x- a tree.
@@ -30,20 +28,20 @@ namespace AdventOfCode.D3
             /// </summary>
             /// <param name="map">Map of slope</param>
             /// <returns>Count of trees in the way</returns>
-            public static int Solve(string map)
+            public static int Solve(string map, Step step)
             {
                 // How many times to go down?
                 // - count of lines.
                 var lines = map.Split(Environment.NewLine);
-                var stepsToMake = map.Length / stepsDown;
+                var stepsToMake = map.Length / step.Down;
                 var rightTotal = 0;
                 var totalTrees = 0;
 
-                for (var downTotal = stepsDown; downTotal < stepsToMake; downTotal+= stepsDown)
+                for (var downTotal = step.Down; downTotal < stepsToMake; downTotal+= step.Down)
                 {
                     // Where will we land to the right?
                     // - map[next][totalStepsToRight % max[next].length]
-                    rightTotal += stepsRight;
+                    rightTotal += step.Right;
                     var isTree = IsDescendAtTree(lines, downTotal, rightTotal);
                     totalTrees += isTree ? 1 : 0;
                 }
@@ -72,15 +70,42 @@ namespace AdventOfCode.D3
             /// It's either x- a tree.
             /// Or 0- an empty space.
             /// Descending down the slope starts from top left corner.
-            /// It is done moving 3 steps right and 1 step down.
-            /// Return the count of trees stepped on traversing the slope in such a way.
+            /// It is done moving x steps right and y step down.
+            /// The combos are: {1,1}, {3,1}, {5,1}, {7,1}, {1,2}.
+            /// Return the count of trees stepped on traversing the slope in each a way multiplied.
             /// </summary>
             /// <param name="map">Map of slope</param>
             /// <returns>Count of trees in the way</returns>
-            public static int Solve(string map)
+            public static long Solve(string map)
             {
-                return 0;
+                Step[] stepCombos =
+                {
+                    new Step(1, 1),
+                    new Step(3, 1),
+                    new Step(5, 1),
+                    new Step(7, 1),
+                    new Step(1, 2),
+                };
+
+                return stepCombos.Product(s => Part1.Solve(map, s));
             }
+        }
+
+        public readonly struct Step
+        {
+            public readonly int Right;
+            public readonly int Down;
+
+            public Step(int right, int down)
+            {
+                Right = right;
+                Down = down;
+            }
+        }
+
+        public static long Product<T>(this IEnumerable<T> numbers, Func<T, int> output)
+        {
+            return numbers.Aggregate<T, long>(1, (res, val) => res * output(val));
         }
     }
 }
