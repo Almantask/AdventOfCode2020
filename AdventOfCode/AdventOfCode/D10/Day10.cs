bab848585d692d8ccc4f07f58a636f7d111daf3f
Almantas Karpavičius <almantusk@gmail.com>
2020-12-21 07:05:46 +0200

    Refactoring and Day10 prep

---
+++ AdventOfCode/AdventOfCode/D10/Day10.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AdventOfCode.Common;
+
+namespace AdventOfCode.D10
+{
+    public class Day10 : AdventOfCodeDay<Day10.Part1, Day10.Part2>
+    {
+        protected override int Day => 10;
+
+        public class Part1 : ISolution
+        {
+            public long Solve(string input)
+            {
+                return 0;
+            }
+        }
+
+        public class Part2 : ISolution
+        {
+            public long Solve(string input)
+            {
+                return 0;
+            }
+        }
+
+    }
+}
