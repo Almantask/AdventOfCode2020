b0878bfcbb07cd76276b021f8c1cb651665478c9
Almantas Karpavičius <almantusk@gmail.com>
2020-12-21 08:00:55 +0200

    Day10 notes and ncunch added

---
+++ AdventOfCode/AdventOfCode.Tests/D10/Part1Tests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Tests.D10
+{
+    public class Part1Tests
+    {
+        public void Test()
+        {
+            // wrong j.
+            // adapters for j +(1-3)
+            // your device = adapters.J.Max + 3
+            // Outlet- 0j
+            // count differences: 1j, 2j and 3j.
+            // diff 1j * diff 3j
+        }
+    }
+}
